1c1f9ae12dba8837d9d107c81b0808c06fc635fd
Humza Ali <humza.ali557@yahoo.com>
2016-01-01 02:21:30 +0500

    Notification

--- app/src/main/java/com/example/sandroid/myapplication/MainActivity.java
+++ app/src/main/java/com/example/sandroid/myapplication/MainActivity.java
@@ -1,39 +1,54 @@
 package com.example.sandroid.myapplication;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v4.app.NotificationCompat;
+import android.view.View;
+import android.widget.Button;
 
 
-public class MainActivity extends ActionBarActivity {
-
+public class MainActivity extends Activity implements View.OnClickListener {
+    Button btnNotification;
+    Intent intent;
+    PendingIntent pendingIntent;
+    NotificationManager notificationManager;
+    NotificationCompat.Builder notification;
+    Context context;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
+
+    }
+    private void init(){
+        context=MainActivity.this;
+        btnNotification= (Button) findViewById(R.id.tv_1);
+        intent=new Intent(context,MainActivity.class);
+        pendingIntent=PendingIntent.getActivity(context,0,intent,0);
+        btnNotification.setOnClickListener(this);
+        notificationManager= (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        notification=new NotificationCompat.Builder(this).setSmallIcon(android.R.drawable.stat_notify_more)
+        .setContentTitle("Title").setContentText("description").setContentIntent(pendingIntent);
+
     }
 
-
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.tv_1:
+                sendNotification();
+                break;
         }
 
-        return super.onOptionsItemSelected(item);
+    }
+    private void sendNotification(){
+        notificationManager.notify(0,notification.build());
     }
 }
